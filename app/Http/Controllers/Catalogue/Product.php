0ea49718f5f4131a62b69229963224d6b66c1550
Rishni <ashameemeduma@gmail.com>
2023-04-21 15:25:32 +0530

    remove debugging

--- app/Http/Controllers/Catalogue/Product.php
+++ app/Http/Controllers/Catalogue/Product.php
@@ -9,7 +9,6 @@ class Product extends Controller
 {
     public function index()
     {
-        // dd(!empty(session()->get('cart')));
         return view('catalogue.index')
         ->with(['products'=> \App\Models\Product::all(), 'cart' => session()->get('cart')]);
     }
